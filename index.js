929c1b50c562a35092e4d93da3c2618fbcc4c4bf
Hans774882968 <44315461+Hans774882968@users.noreply.github.com>
2020-08-19 15:58:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ $("marquee").mouseover(function(){
 $("marquee").mouseout(function(){
     this.start();
 });
+
+/* 格式化输出日期 */
+let zero_completion = (s,num) => {
+    s = String(s);
+    if(s.length < num) return (new Array(num - s.length + 1)).join("0") + s;
+    return s;
+};
+
+(() => {
+    let date = new Date();
+    let y = zero_completion(date.getFullYear(),4),
+        mo = zero_completion(date.getMonth() + 1,2),
+        d = zero_completion(date.getDate(),2),
+        h = zero_completion(date.getHours(),2),
+        mi = zero_completion(date.getMinutes(),2),
+        s = zero_completion(date.getSeconds(),2);
+    let cur_time = `${y}-${mo}-${d} ${h}:${mi}:${s}`;
+    $("#server-info").text(`Total 0.000000(s) query 0, Server time : ${cur_time}, Gzip enabled`);
+})();
